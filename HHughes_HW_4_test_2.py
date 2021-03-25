9d1468cbf5439afb76320ba63abf7c42d158f34d
clootch <68715725+clootch@users.noreply.github.com>
2021-03-25 12:07:55 -0500

    Update HHughes_HW_4_test_2.py

--- HHughes_HW_4_test_2.py
+++ HHughes_HW_4_test_2.py
@@ -2,7 +2,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 import cv2
 import os
-
+import time
 #Need to find what quantifiers I want for classification. 
 """
 So if i use CV2 to filter out all of the crap, then find the area of the white space in the image (Anything of importance)
@@ -10,9 +10,15 @@ then I could use that to classify if its a worm or not.
 """
 
 def findpath(pathname=""):
+    folders = []
     for name in os.listdir(pathname):
-        print(name)
+        if(name.endswith(".png")):
+            print("This is not a folder.")
+        else:
+            folders.append(name)
+    return folders
     
+#Used for checking if the path is valid.
 def valid_path(pathname=""):
     try:
         os.listdir(pathname)
@@ -20,13 +26,30 @@ def valid_path(pathname=""):
     except: 
         return False
 
+def processing(pathname="",folders=""):
+    images = []
+    temp = []
+    for curfolder in folders: 
+        for filename in os.listdir("{}/{}".format(pathname,curfolder)):
+            temp.append(filename)
+        images.append(temp)
+        temp = []
+    return images
+
+
 if __name__ == "__main__":
+    start_time = time.time()
     pathname = input("What is the Pathname: ")
-    done = False
+    #Checks if this is a valid path. Will continue to ask until its a valid path.
     while True:
         if valid_path(pathname=pathname):
             break
         else:
             pathname = input("That path was invalid, Please enter a new one: ")
     pathname = "D:/Celegans_Train"
-    findpath(pathname)
+    folders = findpath(pathname)
+    #we have now found if there are folders in the path. Now to extract all the images. 
+    filenames = processing(pathname=pathname,folders=folders)
+    for category in filenames:
+        print(len(category))
+    print("Execution Time: {}".format(time.time()-start_time))
